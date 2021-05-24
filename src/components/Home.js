57e2e5248e761e7ddf87924da7399721f2e73cc9
an0n <theblizzzzard@gmail.com>
2021-05-24 15:10:20 -0400

    new

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Section from './Section'
+import styled from 'styled-components';
+
+function Home() {
+    return (
+        <Container>
+            <Section />
+            <Section />
+            <Section />
+            <Section />
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
