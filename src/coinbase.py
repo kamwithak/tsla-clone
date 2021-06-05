5f9f77b809cf423492261d3de74f16659bd66b0f
Kam ~ <theblizzzzard@gmail.com>
2021-06-05 12:49:52 -0400

    Create coinbase.py

---
+++ src/coinbase.py
@@ -0,0 +1,15 @@
+Given a list of buy and sell orders, try to find the maximum profit a trader can make?
+
+Given a list of buying and selling prices, check if by given a new price will there be any match / transaction. buyer and seller both should get the best prices.
+
+Implement an order book whre sellers and buyers are satisfied with the price. (Like the one on Coinbase Pro or other exchanges)
+
+Given a list of buy and sell orders, try to find the maximum profit a trader can make?
+
+After solving the question, I was given few practical real world problems a cryptocurrency exchange faces and was asked how I would solve it.
+
+Connect4 object oriented design
+
+Program a game that plays itself - mostly about OOP.
+
+Given some origin get to another part of a weighted directed graph but do it with the longest path possible.
